37b96c185bc2bdba3a858b6e679db8f30f6afd5a
KeddyB <bokelusi@gmail.com>
2024-01-18 11:42:31 +0100

    tis but a scratch

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import React, { useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import './App.css';
-const alanKey = 'c768747ee275e97df4cd16582fc8e87e2e956eca572e1d8b807a3e2338fdd0dc/stage'
+const alanKey = 'fc2908e4da4c19a6f4cd16582fc8e87e2e956eca572e1d8b807a3e2338fdd0dc/stage'
 function App() {
   useEffect(() => {
     alanBtn({
       key: alanKey,
-      onCommand: (commandData) => {
-        if (commandData.command === 'testCommand') {
-          alert('the command was executed')
+      onCommand: ({ command, articles }) => {
+        if (command === 'newsHeadlines') {
+          console.log(articles)
         }
-      },
-      zIndex: 10
+      }
     })
   }, [])
   return (
