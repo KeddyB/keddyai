3edf475183513cc852a63efb37e78a67d4b3b86e
KeddyB <bokelusi@gmail.com>
2024-01-18 13:00:54 +0100

    this is a commit

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useEffect } from 'react';
 import alanBtn from '@alan-ai/alan-sdk-web';
 
 import './App.css';
-const alanKey = 'fc2908e4da4c19a6f4cd16582fc8e87e2e956eca572e1d8b807a3e2338fdd0dc/stage'
+const alanKey = 'a14944d32ed248e5f4cd16582fc8e87e2e956eca572e1d8b807a3e2338fdd0dc/stage'
 function App() {
   useEffect(() => {
     alanBtn({
       key: alanKey,
       onCommand: ({ command, articles }) => {
-        if (command === 'newHeadlines') {
-          console.log(articles)
+        if (command === 'Hello world') {
+          alert('this is a test')
         }
       }
     })
